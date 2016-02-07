2395c1e53f60370fada2042d112babd4865265fe
David Silver <dsilver@candidatemetrics.com>
2016-02-06 18:39:21 -0800

    implement elasticsearch

--- blog-before/app/models/article.rb
+++ blog-before/app/models/article.rb
@@ -1,4 +1,9 @@
+require 'elasticsearch/model'
+
 class Article < ActiveRecord::Base
   belongs_to :author
   has_many :comments
+
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 end
